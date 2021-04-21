8a4c1475e76c5fc0792d0e821ef79f7eb0466871
Francis Antobre <fantobre@yahoo.com>
2021-04-21 15:13:51 -0700

    Prettier formater

--- app/Http/Livewire/ContactForm.php
+++ app/Http/Livewire/ContactForm.php
@@ -8,7 +8,6 @@ use Livewire\Component;
 
 class ContactForm extends Component
 {
-
     public $firstname;
     public $lastname;
     public $email;
@@ -25,34 +24,33 @@ class ContactForm extends Component
     ];
 
 
-    public function updated($propertyName){
-
+    public function updated($propertyName)
+    {
         $this->validateOnly($propertyName);
     }
 
-   public function submitForm(){
+    public function submitForm()
+    {
+        $contacts = $this->validate();
 
-    $contacts = $this->validate();
 
+        $contacts['firstname'] = $this->firstname;
+        $contacts['lastname'] = $this->lastname;
+        $contacts['email'] = $this->email;
+        $contacts['phone'] = $this->phone;
+        $contacts['message'] = $this->message;
 
-    $contacts['firstname'] = $this->firstname;
-    $contacts['lastname'] = $this->lastname;
-    $contacts['email'] = $this->email;
-    $contacts['phone'] = $this->phone;
-    $contacts['message'] = $this->message;
-
-    //   dd($contacts);
+        //   dd($contacts);
 
 
         Mail::to('webdev@protecdental.com')->send(new ContactMailable($contacts));
 
-         $this->resetForm();
-         session()->flash('success_message', 'Your message is received successfully');
+        $this->resetForm();
+        session()->flash('success_message', 'Your message is received successfully');
+    }
 
-   }
-
-    private function resetForm(){
-
+    private function resetForm()
+    {
         $this->firstname = '';
         $this->lastname = '';
         $this->email = '';
